82bde33a4371e4279061a5a3443ea2415489ee1d
Scott Windsor <swindsor@gmail.com>
2018-12-03 20:15:15 -0800

    Day 2 part 2 (ruby)

--- ruby/day02.rb
+++ ruby/day02.rb
@@ -19,6 +19,29 @@ class Device
     end
     twos * threes
   end
+
+  def self.diff w1, w2
+    w1.each_char
+      .zip(w2.each_char)
+      .map { |a,b| a == b ? 0 : 1 }
+      .reduce(&:+)
+  end
+
+  def self.common w1, w2
+    w1.each_char
+      .zip(w2.each_char)
+      .select { |a,b| a == b }
+      .map{ |a,_| a }
+      .join
+  end
+
+  def self.find_common str
+    str.split(/\s+/)
+      .map(&:chomp)
+      .combination(2)
+      .find { |a,b| diff(a,b) == 1 }
+      .reduce { |a,b| common(a,b) }
+  end
 end
 
 if __FILE__ == $0
@@ -30,9 +53,24 @@ if __FILE__ == $0
       def test_checksum
         assert_equal 12, Device.checksum("abcdef bababc abbcde abcccd aabcdd abcdee ababab")
       end
+
+      def test_word_diff
+        assert_equal 1, Device.diff("fghij", "fguij")
+        assert_equal 2, Device.diff("abcde", "axcye")
+        assert_equal 5, Device.diff("klmno", "pqrst")
+      end
+
+      def test_common
+        assert_equal "fgij", Device.common("fghij", "fguij")
+      end
+
+      def test_find_commond
+        assert_equal "fgij", Device.find_common("abcde fghij klmno pqrst fguij axcye wvxyz")
+      end
     end
   else
     input = ARGF.read
     p Device.checksum input
+    p Device.find_common input
   end
 end
