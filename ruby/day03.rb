c9d3c5834da96dbc698db3aa5386e9d3714a046a
Scott Windsor <swindsor@gmail.com>
2018-12-04 23:05:55 -0800

    Day 3 part 1 working (ruby)

--- ruby/day03.rb
+++ ruby/day03.rb
@@ -1,24 +1,24 @@
 #!/usr/bin/env ruby -w
 
-Claim = Struct.new(:num, :left, :top, :right, :bottom) do
+Claim = Struct.new(:num, :left, :top, :width, :height) do
   def self.find_claims str
     from_str(str)
-      .combination(2)
-      .select { |a,b| a.overlap(b) > 1 }
-      .count
+      .each_with_object(Hash.new(0)) { |claim,hash|
+        claim.width.times { |x|
+          claim.height.times { |y|
+            hash[[claim.left+x,claim.top+y]] += 1
+        }
+      }
+    }.select { |k,v| v > 1 }
+    .count
   end
 
   def self.from_str str
     str.split("\n").map do |line|
       m = line.match(/#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/)
       num, x, y, w, h = m[1..5].map(&:to_i)
-      Claim.new num, x,y,x+w,y+h
+      Claim.new num, x,y,w,h
     end
-  end
-
-  def overlap other
-    [0, [right,other.right].min - [left,other.left].max].max *
-      [0, [bottom,other.bottom].min - [top,other.top].max].max
   end
 end
 
@@ -27,26 +27,20 @@ if __FILE__ == $0
     require "minitest/autorun"
     require "minitest/pride"
 
-    class ClaimTest < Minitest::Test
+    class DeviceTest < Minitest::Test
       INPUT = <<~EOS
                 #1 @ 1,3: 4x4
                 #2 @ 3,1: 4x4
                 #3 @ 5,5: 2x2
               EOS
       def test_parse_claims
-        assert_equal [Claim.new(1, 1, 3, 5, 7),
-                Claim.new(2, 3, 1, 7, 5),
-                Claim.new(3, 5, 5, 7, 7)], Claim.from_str(INPUT)
-      end
-
-      def test_overlap
-        c1 = Claim.new(1, 1, 3, 5, 7)
-        c2 = Claim.new(2, 3, 1, 7, 5)
-        assert_equal 4, c1.overlap(c2)
+        assert_equal [Claim.new(1, 1, 3, 4, 4),
+                Claim.new(2, 3, 1, 4, 4),
+                Claim.new(3, 5, 5, 2, 2)], Claim.from_str(INPUT)
       end
 
       def test_overlap_counts
-        assert_equal 1, Claim.find_claims(INPUT)
+        assert_equal 4, Claim.find_claims(INPUT)
       end
     end
   else
